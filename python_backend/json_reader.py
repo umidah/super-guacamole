9c668446bb73225281de843bfe866ec6e038096a
Emil Lopez <emiljohn24@gmail.com>
2020-08-20 23:00:10 +0800

    JSON array reader added

--- python_backend/json_reader.py
+++ python_backend/json_reader.py
@@ -1,1 +1,10 @@
-
+import json
+import os
+def get_json_as_arr(filename: str):
+	json_file = open(os.path.dirname(__file__) + filename)
+	json_obj = json.load(json_file)
+	if isinstance(json_obj, list):
+		return json_obj
+	else:
+		raise TypeError("JSON top level should be an array")
+	
